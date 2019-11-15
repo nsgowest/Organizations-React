318f4d64636d633df0033ce8474f5e44af1fdc32
Aleksandr Fedorov <nsgowest@gmail.com>
2019-11-15 07:46:00 +0300

    added search logic

--- src/SearchTab.jsx
+++ src/SearchTab.jsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DadataSuggestions from 'react-dadata-suggestions';
+import 'react-dadata-suggestions/dist/styles.css';
 
-function SearchTab() {
-  return (
-    <div className="searchTab">
-      <p>Организация или ИП</p>
-      <input type="text" placeholder="Введите название, ИНН или адрес организации" />
+const token = '68f82416ea144e2a2367805e78ca514040ca42c7';
 
-      <div className="addWrapper">
-        <div className="addIcon" />
-        <span>Для добавления новой организации введите ее название, ИНН или адрес.</span>
+class SearchTab extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: '',
+    };
+  }
+
+  render() {
+    return (
+      <div className="searchTab">
+        <p>Организация или ИП</p>
+        <DadataSuggestions
+          token={token}
+          onSelect={(suggestion) => console.log(suggestion)}
+          placeholder="Введите название, ИНН или адрес организации"
+          service="party"
+          
+          selectedSuggestionFormatter={(suggestion) => `${suggestion.data.name.full}`}
+          onError={(error) => alert(error.message)}
+        />
+        <div className="addWrapper">
+          <div className="addIcon" />
+          <span>Для добавления новой организации введите ее название, ИНН или адрес.</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default SearchTab;
