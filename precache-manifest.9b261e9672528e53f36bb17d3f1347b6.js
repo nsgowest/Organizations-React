5da7d6e884cec8e5408ec5357e46e2f78b3aae87
Aleksandr Fedorov <nsgowest@gmail.com>
2019-11-17 16:37:20 +0300

    Updates

---
+++ precache-manifest.9b261e9672528e53f36bb17d3f1347b6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f77f48417dca3da0847da899ca9d41d",
+    "url": "/Organizations-React/index.html"
+  },
+  {
+    "revision": "ee5d455bdd5e153e35ca",
+    "url": "/Organizations-React/static/css/2.6420b2d8.chunk.css"
+  },
+  {
+    "revision": "8f860eca3dd1fbc1ccf2",
+    "url": "/Organizations-React/static/css/main.275227c7.chunk.css"
+  },
+  {
+    "revision": "ee5d455bdd5e153e35ca",
+    "url": "/Organizations-React/static/js/2.8c065f44.chunk.js"
+  },
+  {
+    "revision": "8f860eca3dd1fbc1ccf2",
+    "url": "/Organizations-React/static/js/main.73976fab.chunk.js"
+  },
+  {
+    "revision": "b57c1023129b6a15948e",
+    "url": "/Organizations-React/static/js/runtime-main.585cd102.js"
+  },
+  {
+    "revision": "961f03f79808faf8e86f2f7c04a05d45",
+    "url": "/Organizations-React/static/media/addIcon.961f03f7.svg"
+  },
+  {
+    "revision": "c1ae67a7f017170e3f2733b81a18f635",
+    "url": "/Organizations-React/static/media/deleteIcon.c1ae67a7.svg"
+  },
+  {
+    "revision": "8beb4b2d39596200d51099b52290cace",
+    "url": "/Organizations-React/static/media/doneIcon.8beb4b2d.svg"
+  },
+  {
+    "revision": "8587c4e495d2655c333291f211d15ab8",
+    "url": "/Organizations-React/static/media/headerLogo.8587c4e4.svg"
+  }
+]);
